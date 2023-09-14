d07a169382e04595171fc6a6b9e6d87181c38a64
LeoSalty66 <leosaltycode@gmail.com>
2023-09-14 17:14:08 -0400

    Future Response Command

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 st.title("Internet Search + Math AI Assistant")
 prompt = st.text_input("Enter any question here:")
+
+# if prompt:
+#  response = llm(prompt)
+#  st.write(response)
